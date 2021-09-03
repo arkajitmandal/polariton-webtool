f504a74b495a3eba78ac0f9100e80e7b2e420f00
arkajitmandal <arkajitmandal@gmail.com>
2021-09-03 02:42:08 -0400

    update

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 
-
+var myChart1, myChart2
 var plot = function(){
-    
-    var X =  [-1.5, -1.25, -1.0, -0.75, -0.5, -0.25, 0, 0.25,0.5, 0.75, 1.0, 1.25, 1.5]
+    var X =  [-1.5, -1.25, -1.0, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5]
     var Y1 = Eg(X)
     var Y2 = Ee(X)
-    var Y3 = Eg1(X) //
+    var Y3 = Eg1(X)  
     //[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-    document.getElementById("myChart").height = "90%";
-    document.getElementById("myChart").width = "90%";
-    var w = document.getElementsByClassName("PES")[0].offsetWidth;
+    document.getElementById("myChart").height = 300;
+    document.getElementById("myChart").width = 300;
     var ctx = document.getElementById('myChart').getContext("2d");
     // Gradient for 1
-    var gradientStroke1 = ctx.createLinearGradient(0, 0, w*0.8, 0);
+    var gradientStroke1 = ctx.createLinearGradient(0, 0, 300, 0);
     gradientStroke1.addColorStop(0, '#000000');
     gradientStroke1.addColorStop(1, '#000000');
 
     // Gradient for 2
-    var gradientStroke2 = ctx.createLinearGradient(0, 0, w*0.8, 0);
+    var gradientStroke2 = ctx.createLinearGradient(0, 0, 300, 0);
     gradientStroke2.addColorStop(0,   '#80b6f4');
 
-    var gradientStroke3 = ctx.createLinearGradient(0, 0, w*0.8, 0);
+    var gradientStroke3 = ctx.createLinearGradient(0, 0, 300, 0);
     gradientStroke3.addColorStop(0,   '#ff6b6b');
     
-    var myChart = new Chart(ctx, {
+    myChart1 = new Chart(ctx, {
         type: 'line',
         data: {
             labels: X,
@@ -108,10 +106,121 @@ var plot = function(){
             }
         }
     });
-    document.getElementById("myChart").height = "90%";
-    document.getElementById("myChart").width = "90%";
+
 
 }
+
+var plot2 = function(){
+    var X =  [-1.5, -1.25, -1.0, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5]
+    var Y1 = Eg(X)
+    var Y2 = Ee(X)
+    var Y3 = Eg1(X)  
+    //[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+    document.getElementById("myChart2").height = 300;
+    document.getElementById("myChart2").width = 300;
+    var ctx = document.getElementById('myChart2').getContext("2d");
+    // Gradient for 1
+    var gradientStroke1 = ctx.createLinearGradient(0, 0, 300, 0);
+    gradientStroke1.addColorStop(0, '#000000');
+    gradientStroke1.addColorStop(1, '#000000');
+
+    // Gradient for 2
+    var gradientStroke2 = ctx.createLinearGradient(0, 0, 300, 0);
+    gradientStroke2.addColorStop(0,   '#80b6f4');
+
+    var gradientStroke3 = ctx.createLinearGradient(0, 0, 300, 0);
+    gradientStroke3.addColorStop(0,   '#ff6b6b');
+    
+    myChart2 = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: X,
+            datasets: [{
+                label: "",
+                borderColor: gradientStroke1,
+                pointBorderColor: gradientStroke1,
+                pointBackgroundColor: gradientStroke1,
+                pointHoverBackgroundColor: gradientStroke1,
+                pointHoverBorderColor: gradientStroke1,
+                pointBorderWidth: 10,
+                pointHoverRadius: 10,
+                pointHoverBorderWidth: 1,
+                pointRadius: 2,
+                fill: false,
+                borderWidth: 4,
+                data: Y1
+            },
+            {
+                label: "",
+                borderColor: gradientStroke2,
+                pointBorderColor: gradientStroke2,
+                pointBackgroundColor: gradientStroke2,
+                pointHoverBackgroundColor: gradientStroke2,
+                pointHoverBorderColor: gradientStroke2,
+                pointBorderWidth: 10,
+                pointHoverRadius: 10,
+                pointHoverBorderWidth: 1,
+                pointRadius: 2,
+                fill: false,
+                borderWidth: 4,
+                data: Y2
+            },
+            {
+                label: "",
+                borderColor: gradientStroke3,
+                pointBorderColor: gradientStroke3,
+                pointBackgroundColor: gradientStroke3,
+                pointHoverBackgroundColor: gradientStroke3,
+                pointHoverBorderColor: gradientStroke3,
+                pointBorderWidth: 10,
+                pointHoverRadius: 10,
+                pointHoverBorderWidth: 1,
+                pointRadius: 2,
+                fill: false,
+                borderWidth: 4,
+                data: Y3
+            }]
+            
+        },
+        options: {          
+            legend: {
+                position: "none"
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        fontColor: "rgba(0,0,0,0.5)",
+                        fontStyle: "bold",
+                        beginAtZero: true,
+                        maxTicksLimit: 5,
+                        padding: 20
+                    },
+                    gridLines: {
+                        drawTicks: false,
+                        display: false
+                    }
+
+                }],
+                xAxes: [{
+                    gridLines: {
+                        zeroLineColor: "transparent"
+                    },
+                    ticks: {
+                        padding: 20,
+                        fontColor: "rgba(0,0,0,0.5)",
+                        maxTicksLimit: 5,
+                        fontStyle: "bold"
+                    }
+                }]
+            }
+        }
+    });
+
+
+}
+
+
 
 // Harmonic Functions
 var Vi = function(R, A, B, R0){
@@ -157,3 +266,14 @@ var Ee = function(R){
     return E 
 }
 
+ 
+function update(chart) {
+    var X =  [-1.5, -1.25, -1.0, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5]
+    var Y1 = Eg(X)
+    var Y2 = Ee(X)
+    var Y3 = Eg1(X)
+    chart.data.datasets[0].data =  Y1
+    chart.data.datasets[1].data =  Y2
+    chart.data.datasets[2].data =  Y3
+    chart.update('none');
+}
