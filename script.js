03759a127d5905d6336813c83ceed3b6133cba34
arkajitmandal <arkajitmandal@gmail.com>
2021-09-03 01:45:05 -0400

    pes

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
 
 
 var plot = function(){
-
-    var X =  [-1.0, -0.75, -0.5, -0.25, 0, 0.25,0.5, 0.75, 1.0]
-    var Y1 = [10, 12, 15, 17, 3, 18, 17, 16, 2, 20]
-    var Y2 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    var Y3 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    
+    var X =  [-1.5, -1.25, -1.0, -0.75, -0.5, -0.25, 0, 0.25,0.5, 0.75, 1.0, 1.25, 1.5]
+    var Y1 = Eg(X)
+    var Y2 = Ee(X)
+    var Y3 = Eg1(X) //
+    //[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
     document.getElementById("myChart").height = "90%";
     document.getElementById("myChart").width = "90%";
     var w = document.getElementsByClassName("PES")[0].offsetWidth;
     var ctx = document.getElementById('myChart').getContext("2d");
     // Gradient for 1
-    var gradientStroke1 = ctx.createLinearGradient(0, 0, w, 0);
-    gradientStroke1.addColorStop(0, '#80b6f4');
-    gradientStroke1.addColorStop(1, '#f49080');
+    var gradientStroke1 = ctx.createLinearGradient(0, 0, w*0.8, 0);
+    gradientStroke1.addColorStop(0, '#000000');
+    gradientStroke1.addColorStop(1, '#000000');
 
     // Gradient for 2
-    var gradientStroke2 = ctx.createLinearGradient(0, 0, w, 0);
+    var gradientStroke2 = ctx.createLinearGradient(0, 0, w*0.8, 0);
     gradientStroke2.addColorStop(0,   '#80b6f4');
-    gradientStroke2.addColorStop(0.1, '#80b6f4');
-    gradientStroke2.addColorStop(0.3, '#80b6f4');
-    gradientStroke2.addColorStop(0.4, '#f49080');
-    gradientStroke2.addColorStop(0.5, '#f49080');
-    gradientStroke2.addColorStop(0.6, '#f49080');
-    gradientStroke2.addColorStop(0.7, '#f49080');
-    gradientStroke2.addColorStop(0.8, '#f49080');
-    gradientStroke2.addColorStop(0.9, '#f49080');
-    gradientStroke2.addColorStop(1.0, '#f49080');
+
+    var gradientStroke3 = ctx.createLinearGradient(0, 0, w*0.8, 0);
+    gradientStroke3.addColorStop(0,   '#ff6b6b');
     
     var myChart = new Chart(ctx, {
         type: 'line',
@@ -62,6 +57,21 @@ var plot = function(){
                 fill: false,
                 borderWidth: 4,
                 data: Y2
+            },
+            {
+                label: "",
+                borderColor: gradientStroke3,
+                pointBorderColor: gradientStroke3,
+                pointBackgroundColor: gradientStroke3,
+                pointHoverBackgroundColor: gradientStroke3,
+                pointHoverBorderColor: gradientStroke3,
+                pointBorderWidth: 10,
+                pointHoverRadius: 10,
+                pointHoverBorderWidth: 1,
+                pointRadius: 2,
+                fill: false,
+                borderWidth: 4,
+                data: Y3
             }]
             
         },
@@ -102,3 +112,48 @@ var plot = function(){
     document.getElementById("myChart").width = "90%";
 
 }
+
+// Harmonic Functions
+var Vi = function(R, A, B, R0){
+    return A + B * (R-R0)**2.0
+}
+// Ground State
+var Eg = function(R){
+    var N = R.length
+    var E = Array(N)
+    for (var i=0;i<N;i++){
+        var v1 = Vi(R[i], 0.049244, 0.183746518,  -0.75)
+        var v2 = Vi(R[i], 0.010657, 0.183746518,  0.85)
+        var d = 0.0734986072
+        E[i] =  (0.5 * (v1 + v2) - (d**2.0 + 0.25 * (v1-v2)**2.0 )**0.5 ) * 27.2114
+    } 
+    return E 
+}
+
+// Ground State +1
+var Eg1 = function(R){
+    var hw = document.getElementById("omega").value
+    var N = R.length
+    var E = Array(N)
+    for (var i=0;i<N;i++){
+        var v1 = Vi(R[i], 0.049244, 0.183746518,  -0.75)
+        var v2 = Vi(R[i], 0.010657, 0.183746518,  0.85)
+        var d = 0.0734986072
+        E[i] =  (0.5 * (v1 + v2) - (d**2.0 + 0.25 * (v1-v2)**2.0 )**0.5 ) * 27.2114 +  4.0 * hw/100.0
+    } 
+    return E 
+}
+
+// Excited State
+var Ee = function(R){
+    var N = R.length
+    var E = Array(N)
+    for (var i=0;i<N;i++){
+        var v1 = Vi(R[i], 0.428129, 0.183746518,  -1.15 )
+        var v2 = Vi(R[i], 0.373005, 0.146997214,  1.25)
+        var d = 0.5144902504
+        E[i] =  (0.5 * (v1 + v2) - (d**2.0 + 0.25 * (v1-v2)**2.0 )**0.5 ) * 27.2114
+    } 
+    return E 
+}
+
